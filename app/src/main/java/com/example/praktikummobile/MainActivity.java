11d669bfd92f4e664129fb538da5ad9428ae2fd2
ekky2412 <ekky.raharjo@gmail.com>
2021-03-25 18:40:55 +0700

    first commit

---
+++ app/src/main/java/com/example/praktikummobile/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.praktikummobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tugas2);
+    }
+}
